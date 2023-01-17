b05f87df2eb60d861266aefc99b962b1929d75f9
Crowdbotics <engineering@crowdbotics.com>
2023-01-17 00:07:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "misty-firefly-dev-57264",
+  url: "https://misty-firefly-dev-57264.botics.co",
+  api: "https://misty-firefly-dev-57264.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
